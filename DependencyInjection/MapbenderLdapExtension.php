92c65526b659f8aa602e7c17941adcd0f2e59e2b
Andrey Oblivantsev <eslider@gmail.com>
2017-02-22 11:32:02 +0100

    Fix to not load "services.xml" by MapbenderLdapExtension

--- DependencyInjection/MapbenderLdapExtension.php
+++ DependencyInjection/MapbenderLdapExtension.php
@@ -17,7 +17,6 @@ class MapbenderLdapExtension extends Extension
     {
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('security.xml');
-        $loader->load('services.xml');
     }
 
     /**
